bbcd4cfc7393786a920ab44a4594bd80c6762a7a
Raghav Chandra <raghav.yo@gmail.com>
2020-01-14 21:49:06 -0700

    Hazelcast queue implementation

--- src/main/java/com/rags/tools/mbq/queue/HazelcastMBQueue.java
+++ src/main/java/com/rags/tools/mbq/queue/HazelcastMBQueue.java
@@ -1,11 +1,19 @@
 package com.rags.tools.mbq.queue;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.query.Predicate;
+import com.hazelcast.query.Predicates;
 import com.rags.tools.mbq.QueueStatus;
 import com.rags.tools.mbq.message.MBQMessage;
 import com.rags.tools.mbq.message.QMessage;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * @author ragha
@@ -13,42 +21,104 @@ import java.util.Map;
  */
 public class HazelcastMBQueue extends AbstractMBQueue {
 
-    public HazelcastMBQueue() {
+    private final HazelcastInstance instance;
 
+    //TODO: Use Entry Processor for Atomicity of operations
+    public HazelcastMBQueue() {
+        Config config = new Config();
+        this.instance = Hazelcast.newHazelcastInstance(config);
     }
 
     @Override
     public MBQMessage get(String queueName, String id) {
+        IMap<String, MBQMessage> iMap = this.instance.getMap(queueName);
+        if (iMap != null) {
+            return iMap.get(id);
+        }
         return null;
     }
 
     @Override
     public List<MBQMessage> get(String queueName, String seqKey, List<QueueStatus> status) {
+        IMap<String, MBQMessage> iMap = this.instance.getMap(queueName);
+        if (iMap != null) {
+            Predicate statusInQuery = Predicates.in("status", status.parallelStream()
+                    .map(st -> (Comparable<QueueStatus>) queueStatus -> st == queueStatus ? 0 : 1).toArray(Comparable[]::new));
+            Predicate seqKeyQuery = Predicates.equal("seqKey", seqKey);
+            return new LinkedList<>(iMap.values(Predicates.and(statusInQuery, seqKeyQuery)));
+        }
         return null;
     }
 
     @Override
     public Map<String, List<String>> getAllPendingIds() {
-        return null;
+        Map<String, MapConfig> mapList = instance.getConfig().getMapConfigs();
+
+        Map<String, List<String>> map = new ConcurrentHashMap<>();
+        mapList.keySet().parallelStream().forEach(qName -> {
+            IMap<String, MBQMessage> iMap = this.instance.getMap(qName);
+            if (iMap != null) {
+                List<MBQMessage> allMessages = new LinkedList<>(iMap.values(Predicates.equal("status", QueueStatus.PENDING)));
+                allMessages.sort((m1, m2) -> Math.toIntExact(m1.getCreatedTimeStamp() - m2.getCreatedTimeStamp()));
+                map.put(qName, allMessages.stream().map(MBQMessage::getId).collect(Collectors.toList()));
+            }
+        });
+
+        return map;
     }
 
     @Override
     public List<MBQMessage> pull(String queueName, List<String> ids) {
+        IMap<String, MBQMessage> iMap = this.instance.getMap(queueName);
+        if (iMap != null) {
+            return new LinkedList<>(iMap.getAll(new HashSet<>(ids)).values());
+        }
         return null;
     }
 
     @Override
     public List<MBQMessage> push(String queueName, List<QMessage> messages) {
+        IMap<String, MBQMessage> iMap = this.instance.getMap(queueName);
+        if (iMap != null) {
+            List<MBQMessage> mbqMessages = createMessages(messages, queueName);
+            iMap.putAll(createMap(mbqMessages));
+            return mbqMessages;
+        }
         return null;
+    }
+
+    private Map<String, MBQMessage> createMap(Collection<MBQMessage> mbqMessages) {
+        return mbqMessages.parallelStream().reduce(new HashMap<String, MBQMessage>(), (acc, msg) -> {
+            acc.put(msg.getId(), msg);
+            return acc;
+        }, (map1, map2) -> map1);
     }
 
     @Override
     public boolean updateStatus(String queueName, List<String> ids, QueueStatus status) {
+        IMap<String, MBQMessage> iMap = this.instance.getMap(queueName);
+        if (iMap != null) {
+            Collection<MBQMessage> allMessages = iMap.getAll(new HashSet<>(ids)).values();
+
+            allMessages.parallelStream().forEach(msg -> msg.updateStatus(status));
+
+            iMap.putAll(createMap(allMessages));
+            return true;
+        }
         return false;
     }
 
     @Override
     public void updateStatus(QueueStatus prevStatus, QueueStatus newStatus) {
+        Map<String, MapConfig> mapList = instance.getConfig().getMapConfigs();
 
+        mapList.keySet().parallelStream().forEach(qName -> {
+            IMap<String, MBQMessage> iMap = this.instance.getMap(qName);
+            if (iMap != null) {
+                Collection<MBQMessage> allMessages = iMap.values(Predicates.equal("status", prevStatus));
+                allMessages.parallelStream().forEach(msg -> msg.updateStatus(newStatus));
+                iMap.putAll(createMap(allMessages));
+            }
+        });
     }
 }
---
+++ src/main/java/com/rags/tools/mbq/server/HazelcastMBQServer.java
@@ -0,0 +1,40 @@
+package com.rags.tools.mbq.server;
+
+import com.rags.tools.mbq.QueueStatus;
+import com.rags.tools.mbq.queue.HazelcastMBQueue;
+import com.rags.tools.mbq.queue.InMemoryMBQueue;
+import com.rags.tools.mbq.queue.MBQueue;
+import com.rags.tools.mbq.queue.pending.InMemoryPendingQueueMap;
+import com.rags.tools.mbq.queue.pending.PendingQueueMap;
+
+public class HazelcastMBQServer extends AbstractMBQueueServer {
+
+    private static final MBQueue QUEUE = new HazelcastMBQueue();
+    private static final PendingQueueMap ALL_PENDING_MESSAGES = new InMemoryPendingQueueMap();
+
+    private static final HazelcastMBQServer INSTANCE = new HazelcastMBQServer();
+
+    private HazelcastMBQServer() {
+
+    }
+
+    public static MBQueueServer getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    void init() {
+        QUEUE.updateStatus(QueueStatus.PROCESSING, QueueStatus.PENDING);
+        QUEUE.getAllPendingIds().forEach((key, val) -> ALL_PENDING_MESSAGES.get(key).addAll(val));
+    }
+
+    @Override
+    protected MBQueue getQueue() {
+        return QUEUE;
+    }
+
+    @Override
+    protected PendingQueueMap getPendingQueueMap() {
+        return ALL_PENDING_MESSAGES;
+    }
+}
