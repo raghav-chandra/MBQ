6934d4a71563060385bd2208acc5d99c480b208f
Raghav Chandra <raghav.yo@gmail.com>
2020-07-10 00:27:03 -0600

    WIP - Queue Console basic Structire

---
+++ console/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Management} from './management'
+
+class Stats extends React.Component {
+    render() {
+        return (<div>
+
+        </div>);
+    }
+}
+
+export class App extends React.Component {
+    render() {
+        return (<div>
+            <Stats />
+            <Management />
+        </div>);
+    }
+}
