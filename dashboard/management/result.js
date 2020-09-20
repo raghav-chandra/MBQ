f0b6fb55982791c70152ad335df6f8c437021147
Raghav Chandra <raghav.yo@gmail.com>
2020-09-20 01:15:17 -0600

    SearchResult Sorting toggle

--- dashboard/management/result.js
+++ dashboard/management/result.js
@@ -3,19 +3,35 @@ import { connect } from 'react-redux';
 
 import { Table } from 'react-bootstrap';
 
-const columns = [
-    {key: 'id', name: 'Id'},
-    {key: 'queue', name: 'Summary'},
-    {key: 'status', name: 'Status'},
-    {key: 'seqKey', name: 'Sequence Key'},
-    {key: 'scheduled', name: 'Scheduled Time'},
-    {key: 'created', name: 'Created Time'},
-    {key: 'updated', name: 'Updated Time'}
-];
-
 class Result extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {key: 'id', prevKey: '', dir: 'A'};
+        this.renderItems = this.renderItems.bind(this);
+    }
+
+    renderItems(key) {
+        let dir =  this.state.key === key ? this.state.dir === 'D' ? 'A' : 'D' : 'A';
+        this.setState({key, dir});
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        let items = props.items;
+        let key = state.key;
+        let dir = state.dir;
+        if(dir === 'A') {
+            items.sort((a,b) => a[key] > b[key] ? 1 : -1)
+        } else {
+            items.sort((a,b) => a[key] < b[key] ? 1 : -1)
+        }
+        return {items, key: state.key, dir};
+    }
+
     render () {
-        let items = this.props.items || [];
+        let items = this.state.items
+        console.log(this.state.key);
+        console.log(items);
         let trs = items.map(item => <tr>
                                         <td>{item.id}</td>
                                         <td>{item.queue}</td>
@@ -26,23 +42,24 @@ class Result extends React.Component {
                                         <td>{item.updatedTimeStamp === 0 ? '' : item.updatedTimeStamp}</td>
                                     </tr>);
 
-        return <div style = {{width: this.props.width || '100%', height: this.props.height || '50%'}}>
-                    <Table striped bordered hover variant="dark" size="sm">
+        return <div style = {{width: this.props.width || '100%', height: this.props.height || '100%'}}>
+                    <h5> Total Items found : {items.length}</h5>
+                    <table className='table table-dark table-sm table-striped table-bordered table-hover'>
                         <thead>
                             <tr>
-                                <th>Id</th>
-                                <th>Queue Name</th>
-                                <th>Status</th>
-                                <th>Sequence</th>
-                                <th>Scheduled At</th>
-                                <th>Created At</th>
-                                <th>Updated At</th>
+                                <th onClick={e=>this.renderItems('id')}>Id</th>
+                                <th onClick={e=>this.renderItems('queue')}>Queue Name</th>
+                                <th onClick={e=>this.renderItems('status')}>Status</th>
+                                <th onClick={e=>this.renderItems('seqKey')}>Sequence</th>
+                                <th onClick={e=>this.renderItems('scheduledAt')}>Scheduled At</th>
+                                <th onClick={e=>this.renderItems('createdTimeStamp')}>Created At</th>
+                                <th onClick={e=>this.renderItems('updatedTimeStamp')}>Updated At</th>
                             </tr>
                         </thead>
                         <tbody>
                             {trs}
                         </tbody>
-                    </Table>
+                    </table>
                </div>;
     }
 }
